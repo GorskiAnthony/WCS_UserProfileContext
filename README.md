96f97d5499225f19699604b15071fe3dfef69c7e
Anthony Gorski <anthony.gorski@wildcodeschool.com>
2022-05-02 12:06:20 +0200

    etape2: import CuurentUserContext and sanitize App.js

--- README.md
+++ README.md
@@ -6,4 +6,6 @@ Nous allons voir les étapes pour refactoriser le code qui, bien qu'il fonctionn
 
 ## Les étapes
 
-1.Tout d'abord, nous savons que nous allons utiliser `context` de react et donc, créer un dossier `context` dans `src` et y créer un fichier `CurrentUserContext.js` qui va contenir notre context. 
+1. Tout d'abord, nous savons que nous allons utiliser `context` de react et donc, créer un dossier `context` dans `src` et y créer un fichier `CurrentUserContext.js` qui va contenir notre context.
+
+2. Nous allons ensuite utiliser notre `CurrentUserContext` dans notre `App.js`, ce qui implique de l'importer et de nettoyer notre `App.js`. Et oui, les useState n'est plus necessaire dans `App.js` car nous avons utilisé notre `CurrentUserContext`.
--- src/App.js
+++ src/App.js
@@ -1,38 +1,23 @@
-import React, {useState} from "react";
+import React from "react";
 import {Routes, Route} from "react-router-dom";
+import {CurrentUserContextProvider} from "./context/CurrentUserContext";
 import Home from "./component/Home";
 import Profile from "./component/Profile";
 import Header from "./component/Header";
 import Footer from "./component/Footer";
 
 export default function App() {
-    const [currentUserName, setCurrentUserName] = useState("");
-    const [currentUserAvatar, setCurrentUserAvatar] = useState("");
 
     return (
-        <>
-            <Header
-                currentUserAvatar={currentUserAvatar}
-                currentUserName={currentUserName}
-            />
+        <CurrentUserContextProvider>
+            <Header/>
             <main>
-
                 <Routes>
                     <Route path="/" element={<Home/>}/>
-                    <Route
-                        path="/profile"
-                        element={
-                            <Profile
-                                currentUserName={currentUserName}
-                                setCurrentUserName={setCurrentUserName}
-                                setCurrentUserAvatar={setCurrentUserAvatar}
-                                currentUserAvatar={currentUserAvatar}
-                            />
-                        }
-                    />
+                    <Route path="/profile" element={<Profile/>} />
                 </Routes>
             </main>
-            <Footer currentUserName={currentUserName}/>
-        </>
+            <Footer/>
+        </CurrentUserContextProvider>
     );
 }
